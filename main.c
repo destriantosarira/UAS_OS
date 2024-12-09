56a5d87980df17aa99271df12e1f24f319dc4fec
destriantosarira <destriantosarira05@gimail.com>
2024-12-09 17:30:52 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "des.h"
+
+int main () 
+{
+  printf("\t\t Library sistem operasi\n");
+  printf("\n ============================== \n");
+  printf("Welcome to converter money");
+  printf("\n ============================== \n");
+  printf("\n nilai dolar ke rupiah \n");
+  rupiah();
+  
+   printf("\n nilai rupiah ke dolar \n");
+  dolar();
+}
